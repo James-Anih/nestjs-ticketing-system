5e713dad2bda078b793b2b8aed721896a9e983ba
jobsworks <uchennaanih16@gmail.com>
2023-06-07 09:04:51 +0100

    added test files

--- src/Auth/auth.controller.spec.ts
+++ src/Auth/auth.controller.spec.ts
@@ -1,19 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 jest.mock('./auth.service.ts');
-describe('AuthService', () => {
-  let service: AuthService;
+describe('AuthController', () => {
+  let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
       providers: [AuthService],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    controller = module.get<AuthController>(AuthController);
   });
-
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(controller).toBeDefined();
   });
 });
